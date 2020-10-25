85566f69f68c9711cba0a442a6021aa4b73da910
user <user@DESKTOP-KJUO3L6>
2020-10-26 00:48:24 +0900

    문자형 연습2 백슬레시 는 키보드 \ 유니코드 사용

---
+++ src/two/characterEx2.java
@@ -0,0 +1,15 @@
+package two;
+
+public class characterEx2 {
+
+	public static void main(String[] args) {
+		char ch1 = '한';  //글자의 유니코드 값으로 16진수 사용,2바이트 사용
+		char ch2 = '\uD55C'; //
+		
+		System.out.print(ch1);
+		System.out.print(ch2);
+		// 문자를 사용할때 항상 작은 따옴표''사용
+
+	}
+
+}
